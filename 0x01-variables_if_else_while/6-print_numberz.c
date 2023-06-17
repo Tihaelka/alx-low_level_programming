0973f8770a3be7bc69a547ae6942aadb24c776ae
Tihaelka <fkassimi50@gmail.com>
2023-06-17 08:10:03 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
@@ -10,7 +12,8 @@ int main(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-	printf("%d", n);
+		putchar(n + 48);
 	}
+	putchar('\n');
 	return (0);
 }
