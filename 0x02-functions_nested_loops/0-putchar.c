fa01a2d9b32273ce9e08f88bfc50d5e04af53544
Tihaelka <fkassimi50@gmail.com>
2023-06-20 02:51:01 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main _ Entry point
+ *Descreption :This program demonstrates the usage of the putchar function
+ *to print an underscore.
+ * Return: On success 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
+
+
+
+
+
+
+
