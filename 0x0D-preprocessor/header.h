ace16621ac4bf1bdefa976ea9b40065c797a7d63
Tihaelka <fkassimi50@gmail.com>
2023-07-17 03:41:27 -0700

    All header files included guarded

--- 0x0D-preprocessor/header.h
+++ 0x0D-preprocessor/header.h
@@ -5,6 +5,16 @@
 #ifndef OBJECT_LIKE_MACRO_H
 #define OBJECT_LIKE_MACRO_H
 
+#ifndef PI_H
+#define PI_H
+
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#ifndef SUM_H
+#define SUM_H
+
+
 
 
 
