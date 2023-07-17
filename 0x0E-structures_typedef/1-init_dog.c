12bf2b167fa583db587437ff1ce3539d3444cc35
Tihaelka <fkassimi50@gmail.com>
2023-07-17 12:47:52 -0700

    A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,32 +1,19 @@
-#ifndef DOG_H
-#define DOG_H
 #include <stdlib.h>
+#include "dog.h"
 
 /**
- * init_dog - Initialise une structure de chien
- * avec les valeurs spécifiées
- * @d: Pointeur vers la structure de chien à initialiser
- * @name: Nom du chien
- * @age: Âge du chien
- * @owner: Propriétaire du chien
- * Description :
- * La fonction init_dog initialise une structure
- * de chien avec les valeurs spécifiées
- * pour le nom, l'âge et le propriétaire.
- * Elle met à jour les champs de la structure
- * à l'aide du pointeur fourni.
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog;
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
 
-#endif
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -23,3 +23,6 @@ void print_dog(struct dog *d)
 
 
 
+
+
+
