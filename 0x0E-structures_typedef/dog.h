19661992eb884e221e4b32d3785d337eeb98d58c
Tihaelka <fkassimi50@gmail.com>
2023-07-17 13:51:28 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,25 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * dog_t - Typedef for the struct dog type.
+ * struct dog - Represents a dog with its attributes
+ * @name: The name of the dog (string)
+ * @age: The age of the dog (float)
+ * @owner: The owner of the dog (string)
  *
  * Description:
- * The `dog_t` typedef is used to create a new name
- * for the `struct dog` type.
- * This allows for easier and more readable declaration
- * of variables of type `struct dog
+ * The struct dog represents a dog with its name, age, and owner's name.
+ * It is used to store information about a specific dog.
  */
-typedef struct
+struct dog
 {
-	char *name;
+	char  *name;
 	float age;
-	char *owner;
-} dog_t;
-
+	char  *owner;
+} dog;
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
+
+
